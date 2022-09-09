b4ad2f08919578695508b43929ba8529d7a66af5
Ikenna Udemezue <iykethe1st@gmail.com>
2022-09-09 10:49:11 -0700

    Task 6 commit

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,11 +9,9 @@
 
 int main(void)
 {
-    int n;
-    for(n = 0; n < 10; n++)
-	   {
+	int n;
+	for (n = 0; n < 10; n++)
 		putchar(n + '0');
-	   }
-    	   putchar('\n');
-	   return (0);
+	putchar('\n');
+	return (0);
 }
