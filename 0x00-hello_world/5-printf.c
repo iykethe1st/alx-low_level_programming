57ed1549902dfd167fa763f190fc064eb0a5d7c9
Ikenna Udemezue <iykethe1st@gmail.com>
2022-09-08 21:15:14 -0700

    Task 5 commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
  * main - Main block
- * Return: 0
+ * Return: Always 0 (success)
  */
-int main(void)
+int main(voi
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);	
