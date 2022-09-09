800b7742c6adb9479c7fc028926c1721e46553a6
Ikenna Udemezue <iykethe1st@gmail.com>
2022-09-08 21:43:38 -0700

    Task 6 commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,11 +4,12 @@
  * Return: Always 0 (Success)
  */
 
-int main (void)
+int main(void)
 {
-	printf("The size of a char is: %i bytes\n", sizeof(char));
-	printf("The size of an int is: %i bytes\n", sizeof(int));
-	printf("The size of a float is: %i bytes\n", sizeof(float));
-	printf("The size of double data type is: %i bytes\n", sizeof(double));
+	printf("The size of a char: %i bytes(s)\n", sizeof(char));
+	printf("The size of an int: %i bytes(s)\n", sizeof(int));
+	printf("The size of a float: %i bytes(s)\n", sizeof(float));
+	printf("The size of a long int: %i bytes(s)\n", sizeof(long int));
+	printf("The size of long long int: %i bytes(s)\n", sizeof(long long int));
 	return (0);
 }
