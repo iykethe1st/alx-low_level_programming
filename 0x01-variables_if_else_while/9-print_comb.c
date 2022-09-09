945716699a4375d260c98b96c0dba812eece731f
Ikenna Udemezue <iykethe1st@gmail.com>
2022-09-09 11:17:31 -0700

    Task 9 commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,20 +12,18 @@
 
 int main(void)
 {
-    int number;
+	int number;
 
-    for(number = 0; number < 10; number++)
-    {
-	putchar(number + '0');
-	
-	if(number != 9)
+	for(number = 0; number < 10; number++)
 	{
-	   putchar(',');
-	   putchar(' ');
-	}
-	
-    }
-    putchar('\n');
-    return (0);
+		putchar(number + '0');
 
+		if(number != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
