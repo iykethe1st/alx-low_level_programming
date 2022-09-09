d09267edd8b6aa31f4281594eb9c2b02ebc76c41
Ikenna Udemezue <iykethe1st@gmail.com>
2022-09-09 11:28:41 -0700

    Task 10 commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,17 +18,15 @@ int main(void)
 {
 	int a, b, c;
 
-	for(a = 0; a < 100; a++)
+	for (a = 0; a < 100; a++)
 	{
 		b = a % 10;
 		c = a / 10;
-
 		if(c < b)
 		{
 			putchar(c + '0');
 			putchar(b + '0');
-
-			if(a != 89)
+			if (a != 89)
 			{
 				putchar(',');
 				putchar(' ');
