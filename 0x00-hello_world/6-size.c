2cdf8d1af62690dfcde79af35e3d45babc001a7d
Ikenna Udemezue <iykethe1st@gmail.com>
2022-09-08 21:35:52 -0700

    Task 6 commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Return: Always 0 (Success)
+ */
+
+int main (void)
+{
+	printf("The size of a char is: %i bytes\n", sizeof(char));
+	printf("The size of an int is: %i bytes\n", sizeof(int));
+	printf("The size of a float is: %i bytes\n", sizeof(float));
+	printf("The size of double data type is: %i bytes\n", sizeof(double));
+	return (0);
+}
