1c81b44a04a4342802738f86074c3170ae51ad8d
Ikenna Udemezue <iykethe1st@gmail.com>
2022-09-09 12:22:51 -0700

    Task 11 commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,26 +13,25 @@ int main(void)
 {
 	int a, b, c, d;
 
-	for(a = 0; a < 1000; a++)
+	for (a = 0; a < 1000; a++)
 	{
-	    b = a % 10;
-	    c = (a / 10) % 10;
-	    d = a / 100;
+		b = a % 10;
+		c = (a / 10) % 10;
+		d = a / 100;
 
-	    if(d < c && c < b)
-	    {
-		putchar(d + '0');
-		putchar(c + '0');
-		putchar(b + '0');
-		
-		if(a != 789)
+		if (d < c && c < b)
 		{
-		    putchar(',');
-		    putchar(' ');
+			putchar(d + '0');
+			putchar(c + '0');
+			putchar(b + '0');
+
+			if (a != 789)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
-	    }
 	}
 	putchar('\n');
-	return(0);
-
+	return (0);
 }
