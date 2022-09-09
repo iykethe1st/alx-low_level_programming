df0a672456df0c79a3d3a2df56461b26a7120d93
Ikenna Udemezue <iykethe1st@gmail.com>
2022-09-09 12:15:30 -0700

    Task 12 commit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int a, b, c, d;
+	int i, j;
+
+	for (i = 0; i < 100; i++)
+	{
+		a = i % 10;
+		b = i / 10;
+		for ( j = 0; j < 100; j++)
+		{
+			c = j % 10;
+			d = j / 10;
+
+			if (b < d || (b == d && a < c))
+			{
+				putchar(b + '0');
+				putchar(a + '0');
+				putchar(' ');
+				putchar(d + '0');
+				putchar(c + '0');
+
+				if (!(b == 9 && a == 8))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
