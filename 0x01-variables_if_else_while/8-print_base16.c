428ad222993d5e7b997cbe758af5d4ff6c98b0c7
Ikenna Udemezue <iykethe1st@gmail.com>
2022-09-09 11:10:20 -0700

    Task 8 commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,13 +9,13 @@
 
 int main(void)
 {
-    int digit;
-    char alphabet;
+	int digit;
+	char alphabet;
 
-    for (digit = 0; digit < 10; digit++)
-	putchar(digit + '0');
-    for (alphabet = 'a'; alphabet < 'g'; alphabet++)
-        putchar(alphabet);
-    putchar('\n');
-    return (0);
+	for (digit = 0; digit < 10; digit++)
+		putchar(digit + '0');
+	for (alphabet = 'a'; alphabet < 'g'; alphabet++)
+		putchar(alphabet);
+	putchar('\n');
+	return (0);
 }
