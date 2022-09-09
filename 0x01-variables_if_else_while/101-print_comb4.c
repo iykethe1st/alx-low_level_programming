3c1d9c8f06513ac1035cb669565744daf69a6afc
Ikenna Udemezue <iykethe1st@gmail.com>
2022-09-09 09:07:32 -0700

    Task 0 commit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: 0;
+ *
+ *
+ */
+
+
+int main(void)
+{
+	int a, b, c, d;
+
+	for(a = 0; a < 1000; a++)
+	{
+	    b = a % 10;
+	    c = (a / 10) % 10;
+	    d = a / 100;
+
+	    if(d < c && c < b)
+	    {
+		putchar(d + '0');
+		putchar(c + '0');
+		putchar(b + '0');
+		
+		if(a != 789)
+		{
+		    putchar(',');
+		    putchar(' ');
+		}
+	    }
+	}
+	putchar('\n');
+	return(0);
+
+}
