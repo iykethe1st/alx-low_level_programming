ed2415bbc2c2b4eea5896870a66b664ef9715837
Ikenna Udemezue <iykethe1st@gmail.com>
2022-09-13 07:53:12 -0700

    Task 7 commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * 
+ * @n: int type number
+ *
+ * Return: return the value of the last digit
+ *
+ */ 
+
+int print_last_digit(int n)
+{
+	int lastdigit;
+
+	if (n < 0)
+	{
+		lastdigit = -1 * (n % 10);
+		_putchar(lastdigit + '0');
+		return lastdigit;
+	}
+	lastdigit = n % 10;
+	_putchar(lastdigit + '0');
+	return lastdigit;
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -34,5 +34,10 @@ Prototype: int _abs(int);
 FYI: The standard library provides a similar function: abs. Run man abs to learn more.
 
 
+Task 7: function that prints the last digit of a number.
+Prototype: int print_last_digit(int);
+Returns the value of the last digit
+
+
 
 
