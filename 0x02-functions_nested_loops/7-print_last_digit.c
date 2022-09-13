55ac8dc7cb91ff24eda4f8e6c291d5718c6e2951
Ikenna Udemezue <iykethe1st@gmail.com>
2022-09-13 09:29:24 -0700

    Task 7 commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -18,12 +18,12 @@ int print_last_digit(int n)
 	{
 		lastdigit = -1 * (n % 10);
 		_putchar(lastdigit + '0');
-		return lastdigit;
+		return (lastdigit);
 	}
 	else
 	{
 		lastdigit = n % 10;
 		_putchar(lastdigit + '0');
-		return lastdigit;
+		return (lastdigit);
 	}
 }
