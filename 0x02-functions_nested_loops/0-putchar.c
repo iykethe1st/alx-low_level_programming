810f77b065c5b0db021b4c0c2a40ab2f911ff050
Ikenna Udemezue <iykethe1st@gmail.com>
2022-09-13 00:48:23 -0700

    Task 0 commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+
+/**
+ * _putchar - prints the character '_putchar' to stdout
+ *
+ * main - Entry Point
+ *
+ * Return: on success 1
+ *
+ * on error: -1
+ *
+ */
+
+int _putchar(void)
+{
+	putchar('_putchar');
+	putchar('\n');
+
+	return (0);
+}
+
+int main(void)
+{
+	_putchar();
+
+	return (0)
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,5 @@
+Task 0: Write a program that prints _putchar, followed by a new line.
+The program should return 0
+
+
+
