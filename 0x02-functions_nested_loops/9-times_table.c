ea948b0db80bb7d095bbd387bcd9d7a5e9c6b979
Ikenna Udemezue <iykethe1st@gmail.com>
2022-09-13 20:16:03 -0700

    Task 9 commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * times_table - function prints the 9 times table
+ *
+ * Return: 9 times table
+ */
+
+void times_table(void)
+{
+	int row;   /* Row */
+	int column; /* Column */
+	int product; /* Product - computed value */
+
+	for (row = 0; row < 10; row ++)
+	{
+		for (column = 0; column < 10; column++)
+		{
+			product = row * column;
+
+			if (product < 10)
+			{
+				_putchar(' ');
+				_putchar(product + '0');
+			}
+			else if (product > 9)
+			{
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+			if (column != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
