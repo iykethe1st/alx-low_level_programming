770ccc213ca4a915ec8ea9dfde72866bb73f7ae4
Ikenna Udemezue <iykethe1st@gmail.com>
2022-09-13 20:45:17 -0700

    Task 10 commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ *
+ * @x: Integer to be added
+ *
+ * @y: integer to be added
+ *
+ * Return: the sum of both integers
+ */
+
+int add(int x, int y)
+{
+	int sum = x + y;
+	return (sum);
+}
