df5ed418b4b0d33df32ea049c0716deb8535474d
Ikenna Udemezue <iykethe1st@gmail.com>
2022-09-13 20:33:21 -0700

    Task 9 commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,15 +19,15 @@ void times_table(void)
 		{
 			product = row * column;
 
-			if (product < 10)
-			{
-				_putchar(' ');
-				_putchar(product + '0');
-			}
-			else if (product > 9)
+			if (product > 9)
 			{
 				_putchar((product / 10) + '0');
 				_putchar((product % 10) + '0');
+			}
+			else if (product < 10)
+			{
+				_putchar(' ');
+				_putchar(product + '0');
 			}
 			else 
 			{
