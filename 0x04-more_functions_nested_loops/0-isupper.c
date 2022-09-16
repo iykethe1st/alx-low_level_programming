7277c4fc5733a225c029ad1f750a912ea5dae356
Ikenna Udemezue <iykethe1st@gmail.com>
2022-09-15 19:05:40 -0700

    Task 0 commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+/**
+ * _isupper - checks for uppercase character.
+ *
+ * @c: Character to be checked
+ *
+ * Return: 1 if c is uppercase, otherwise 0
+ */
+
+int _isupper(int c)
+{
+
+	if (c >= 'A' && c <= 'Z' )
+		return (1);
+	return (0);
+}
