de18b674554096d77b72fb70c12232e99ed6b2e1
Ikenna Udemezue <iykethe1st@gmail.com>
2022-09-15 19:57:34 -0700

    Task 4 commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print digit character
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	char j;
+
+	for (j = '0'; j <= '9'; j++)
+	{
+		if (!(j == '2' || j == '4'))
+			_putchar(j);
+
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -21,5 +21,11 @@ You can only use _putchar twice in your code
 
 
 
+Task 4: unction that prints the numbers, from 0 to 9, followed by a new line.
+Prototype: void print_most_numbers(void);
+Do not print 2 and 4
+You can only use _putchar twice in your code
+
+
 
 
