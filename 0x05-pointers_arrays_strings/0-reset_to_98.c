4c0fe29405204260e91fd807a73aa900439bb2c3
Ikenna Udemezue <iykethe1st@gmail.com>
2022-09-19 08:19:01 -0700

    Initialization and Task 0 commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+/**
+ * reset_to_98 - takes a pointer and update the value it point to
+ *
+ * @n: parameter passed in
+ *
+ * Return: Void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
