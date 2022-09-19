fc9e54ff84ecc2272650c4e7ae7aa5ac17fc7d55
Ikenna Udemezue <iykethe1st@gmail.com>
2022-09-19 08:53:01 -0700

    Task 2 commit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
  * _strlen - returns the length of a string
@@ -12,8 +11,8 @@
 int _strlen(char *s)
 {
 	int result;
-	
-	for (result = 0; s[result] != '\0'; result++) 
+
+	for (result = 0; s[result] != '\0'; result++)
 		;
 	return (result);
 }
