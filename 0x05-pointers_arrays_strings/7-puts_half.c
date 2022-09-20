ceedc9fff529cd3cb83260b37ce37fa237cf01c0
Ikenna Udemezue <iykethe1st@gmail.com>
2022-09-19 18:10:17 -0700

    Task 7 commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,15 +16,9 @@ void puts_half(char *str)
 	for (len = 0; str[len] != '\0'; len++)
 		;
 	
-	if (len % 2 == 0)
+	for (i = len / 2; str[i] != '\0'; i++)
 	{
-		for (i = len / 2; i < len; i++)
-			_putchar(str[i]);
-	}
-	else 
-	{
-		for (i = (len - 1) / 2; i < len; i++)
-			_putchar(str[i]);
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 
