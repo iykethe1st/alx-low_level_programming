9441047e0a845be537ca85fe03342a50c0fa700c
Ikenna Udemezue <iykethe1st@gmail.com>
2022-09-19 17:20:53 -0700

    Task 4 and 5 commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rev_string - function that reverses a string.
+ *
+ * @s: string to reverse
+ *
+ * Return: Voin
+ */
+
+void rev_string(char *s)
+{
+	char temp;
+
+	int i;
+	int l;	
+	int c;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	
+	l = i;
+	
+	for (c = 0; c < l / 2; c++)
+	{
+		temp = s[c];
+		s[c] = s[l - c - 1];
+		s[l - c - 1] = temp;
+	}
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -13,4 +13,12 @@ Task 3: function that prints a string, followed by a new line, to stdout.
 Prototype: void _puts(char *str);
 
 
+Task 4: function that prints a string, in reverse, followed by a new line.
+Prototype: void print_rev(char *s);
+
+
+Task 5: function that reverses a string.
+Prototype: void rev_string(char *s);
+
+
 
