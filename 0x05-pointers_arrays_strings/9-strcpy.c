31844bd39d6b698ba2262a77b35f584202c051de
Ikenna Udemezue <iykethe1st@gmail.com>
2022-09-19 19:29:11 -0700

    Task 8 and 9 commit

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * *_strcpy -  copies the string pointed to by src
  *
@@ -13,7 +14,7 @@ char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	for (i=0; src[i] != '\0'; i++)
+	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
