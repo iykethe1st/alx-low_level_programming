accd414735a482fc49542ed050b6d02412756d48
Ikenna Udemezue <iykethe1st@gmail.com>
2022-09-19 18:58:17 -0700

    Task 8 commit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,14 +14,17 @@
 void print_array(int *a, int n)
 {
 	int i;
-	
+
 	i = 0;
+
 	for (n--; n >= 0; n--, i++)
 	{
 		printf("%d", a[i]);
-		
+
 		if (n > 0)
+		{
 			printf(", ");
+		}
 	}
 	printf("\n");
 }
