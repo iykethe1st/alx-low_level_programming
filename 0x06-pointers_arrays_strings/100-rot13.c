b08b64549dee5f73402157d061208ec1221ae9da
Ikenna Udemezue <iykethe1st@gmail.com>
2022-09-21 13:57:19 -0700

    Task 8 commit

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *rot13 - function that encodes a string using rot13.
+ *
+ * @s: string to be encoded
+ *
+ * Return: New encoded string
+ */
+
+char *rot13(char *s)
+{
+	int i;
+
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if ((s[i] >= 65 && s[i] <= 77) || (s[i] >= 97 && s[i] <= 109))
+		{
+			s[i] += 13;
+		}
+		else if ((s[i] >= 110 && s[i] <= 122) || (s[i] >= 78 && s[i] <= 90))
+		{
+			s[i] -= 13;
+		}
+	}
+	return (s);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -52,3 +52,18 @@ You can only use one if in your code
 You can only use two loops in your code
 You are not allowed to use switch
 You are not allowed to use any ternary operation
+
+
+
+Task 8: function that encodes a string using rot13.
+Prototype: char *rot13(char *);
+You can only use if statement once in your code
+You can only use two loops in your code
+You are not allowed to use switch
+You are not allowed to use any ternary operation
+
+
+
+
+
+
