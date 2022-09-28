a3ef6c8e9dc81d269fade35ebfae29e4002965dd
Ikenna Udemezue <iykethe1st@gmail.com>
2022-09-28 08:39:25 -0700

    Task 2 commit

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ *
+ * @s: pointer to string input
+ *
+ * Return: The length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s != '\0')
+	{
+		i = 1 + _strlen_recursion(s + 1);
+	}
+	return (i);
+
+}
