36b993094bc66806205c28b9772232a3dc376b88
Ikenna Udemezue <iykethe1st@gmail.com>
2022-10-01 09:26:51 -0700

    Task 3 commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,11 +9,18 @@
  *
  * @argv: pointer to array that holds arguments
  *
+ * Return: 0 on success, Error on failure
  */
 
 int main(int argc, char *argv[])
 {
 	(void) argc;
+
+	if (!(argv[1] && argv[2]))
+	{
+		printf("Error\n");
+		return (1);
+	}
 
 	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 
