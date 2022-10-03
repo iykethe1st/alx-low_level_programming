e83308149c3c5ab5abd3181d57201c913a1d5038
Ikenna Udemezue <iykethe1st@gmail.com>
2022-10-03 09:08:45 -0700

    Task 2 commit

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenates two strings
+ *
+ * @s1: string to be concatenated to
+ *
+ * @s2: string to be concatenated
+ *
+ * Return: New concatenated string on success, NULL on failure
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i, j, a, b;
+
+	char *str;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+
+	str = malloc((i + j) * sizeof(char) + 1);
+
+	for (a = 0, b = 0; a < (i + j + 1); a++)
+	{
+		if (a < i)
+			str[a] = s1[a];
+		else
+			str[a] = s2[b++];
+	}
+
+	return (str);
+
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -17,5 +17,10 @@ On success, the _strdup function returns a pointer to the duplicated string. It 
 
 
 
+Task 2: function that concatenates two strings.
 
+Prototype: char *str_concat(char *s1, char *s2);
+The returned pointer should point to a newly allocated space in memory which contains the contents of s1, followed by the contents of s2, and null terminated
+if NULL is passed, treat it as an empty string
+The function should return NULL on failure
 
