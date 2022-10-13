ed9cdad9c583ac5935a3f1ddd5ced130b70effa8
Ikenna Udemezue <iykethe1st@gmail.com>
2022-10-12 21:37:59 -0700

    Hot fix

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,5 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
-#include <stddef.h>
+#ifndef _FUNCTION_POINTERS_
+#define _FUNCTION_POINTERS_
 #include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
