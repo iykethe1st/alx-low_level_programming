a05fdadafabc7aecc161334628d6626adb3666b6
Ikenna Udemezue <iykethe1st@gmail.com>
2022-10-31 13:41:07 -0700

    Task 0 commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * read_textfile -  reads a text file and prints it to the POSIX standard output.
- * 
- * @filename: name of the file 
+ * read_textfile -  reads a text file and prints
+ * it to the POSIX standard output.
+ *
+ * @filename: name of the file
  *
  * @letters:  number of letters to read and print
  *
