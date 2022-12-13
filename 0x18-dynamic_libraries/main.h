3039a9262ba246117aea1336e6f1be2b651a7606
Ikenna Udemezue <iykethe1st@gmail.com>
2022-12-12 17:36:15 -0800

    hot fix

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,4 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
 
 int _putchar(char c);
 int _islower(int c);
@@ -22,4 +21,5 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif
+#define MAIN_H
+#endif /*MAIN_H*/
